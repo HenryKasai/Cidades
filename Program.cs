dae86915eeb0f88dbff94b78023bce9d02e11c4a
HenryKasai <henrykasai1775@gmail.com>
2020-02-28 20:21:28 -0300

    Solução do Exercício

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A seguir, as cidades do litoral sul de São Paulo:");
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Peruíbe");
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Iguape");
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Cananéia");
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Ilha Comprida");
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Registro");
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Miracatu");
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Juquiá");
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Itariri");
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Eldorado");
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Jacupiranga");
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Pedro de Toledo");
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Sete Barras");
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Cajati");
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Pariquera-Açu");
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadKey();
+            Console.WriteLine("Barra do Turvo");
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadKey();
+
+
+           
+
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Cidades
 Cidades do litoral sul de São Paulo
+
+Execução de exercícios:
+
+Faça um programa que exiba os nomes das cidades do litoral sul de São Paulo, um em cada linha.
